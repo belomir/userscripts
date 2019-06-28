0120a725a3d7edbff98ee07a167d6acb43a21f12
belomir <leggis@mail.ru>
2019-06-28 13:18:52 +0700

    Add files via upload

--- scormProperties.user.js
+++ scormProperties.user.js
@@ -10,24 +10,123 @@
 exec();
 
 
-function getId(){
+function getParameters(){
+	let currentModuleName = document.querySelector('input[name="modulename"]').value;
 	let current = document.querySelector('.current_branch');
 	let previous = current.previousElementSibling;
+	let previousId;
+	let previousModuleName;
 	if(previous){
 		let a = previous.querySelector('a');
 		let href = a.href;
 		let url = new URL(href);
 		let searchParams = url.searchParams;
-		let id = searchParams.get('id');
-		return id;
-	}else{
-		return false
+		previousId = searchParams.get('id');
+		let matching = href.match(/mod\/([^\/]*)\/view/);
+		if(matching.length>1){
+			previousModuleName = matching[1];
+		}
+	};
+	return {
+		previousId: previousId,
+		previousModuleName: previousModuleName,
+		currentModuleName: currentModuleName
 	};
 }
 
 
-function setProperties(id){
-	/* view */
+function setProperties({previousId, previousModuleName, currentModuleName}){
+	/* properties */
+	switch(currentModuleName){
+		case 'scorm':
+			setScormProperties(previousId);
+			break;
+	}
+	/* completion */
+	switch(currentModuleName){
+		case 'page':
+		case 'resource':
+			setSimpleCompletion(previousId);
+			break;
+		case 'scorm':
+			setScormCompletion(previousId);
+			break;
+	}
+	/* availability */
+	switch(previousModuleName){
+		case 'page':
+		case 'resource':
+			setSimpleAvailability(previousId);
+			break;
+		case 'scorm':
+			setGradeAvailability(previousId);
+			break;
+	}
+}
+
+
+function setSimpleAvailability(id){
+	if(id){
+		document.querySelector('#id_availabilityconditionsheader button').click();
+		document.querySelector('#availability_addrestriction_completion').click();
+		document.querySelector('select[name="cm"]').value = id;
+		document.querySelector('select[name="e"]').value = 0;
+		document.querySelector('#id_availabilityconditionsjson').value = `{"op":"&","c":[{"type":"completion","cm":${id},"e":0}],"showc":[true]}`;
+	}
+}
+
+
+function setGradeAvailability(id){
+	if(id){
+		document.querySelector('#id_availabilityconditionsheader button').click();
+		document.querySelector('#availability_addrestriction_completion').click();
+		document.querySelector('select[name="cm"]').value = id;
+		document.querySelector('select[name="e"]').value = 2;
+		document.querySelector('#id_availabilityconditionsjson').value = `{"op":"&","c":[{"type":"completion","cm":${id},"e":2}],"showc":[true]}`;
+	}
+}
+
+
+function setSimpleCompletion(id){
+	let needToUnlock = false;
+	let completion = document.querySelector('#id_completion');
+	if(completion.value != 2){
+		needToUnlock = true;
+		completion.value = 2;
+	}
+	let completionview = document.querySelector('#id_completionview');
+	if(!completionview.checked){
+		needToUnlock = true;
+		completionview.checked = true;
+		completionview.disabled = false;
+	};
+	if(needToUnlock)
+		document.querySelector('input[name="completionunlocked"]').value = 1;
+}
+
+
+function setScormCompletion(){
+	let needToUnlock = false;
+	let completion = document.querySelector('#id_completion');
+	if(completion.value != 2){
+		needToUnlock = true;
+		completion.value = 2;
+	}
+	let completionusegrade = document.querySelector('#id_completionusegrade');
+	if(completionusegrade.value != 1){
+		needToUnlock = true;
+		completionusegrade.disabled = false;
+		completionusegrade.value = 1;
+		completionusegrade.checked = true;
+		document.querySelector('#id_completionscoredisabled').checked = true;
+		document.querySelector('#id_completionscorerequired').disabled = true;
+	}
+	if(needToUnlock)
+		document.querySelector('input[name="completionunlocked"]').value = 1;
+}
+
+
+function setScormProperties(info){
 	document.querySelector('#id_popup').value = '1';
 	document.querySelector('#id_width').disabled = false;
 	document.querySelector('#id_width').value = '100%';
@@ -37,31 +136,34 @@ function setProperties(id){
 	document.querySelector('#id_displayactivityname').checked = false;
 	document.querySelector('#id_hidetoc').value = 3;
 	document.querySelector('#id_nav').disabled = true;
-	/* completion */
-	document.querySelector('#id_completion').value = 2;
-	document.querySelector('#id_completionusegrade').disabled = false;
-	document.querySelector('#id_completionusegrade').value = 1;
-	document.querySelector('#id_completionusegrade').checked = true;
-	document.querySelector('#id_completionscoredisabled').checked = true;
-	document.querySelector('#id_completionscorerequired').disabled = true;
-	/* availability */
-	if(id){
-		document.querySelector('#id_availabilityconditionsheader button').click();
-		document.querySelector('#availability_addrestriction_completion').click();
-		document.querySelector('select[name="cm"]').value = id;
-		document.querySelector('select[name="e"]').value = 2;
-		document.querySelector('#id_availabilityconditionsjson').value = `{"op":"&","c":[{"type":"completion","cm":${id},"e":2}],"showc":[true]}`;
-	}
-	// document.querySelector('#id_submitbutton2').click();
+}
+
+
+function submit(){
+	document.querySelector('#id_submitbutton2').click();
 }
 
 
 function exec(){
+	let parameters = getParameters();
+	console.log(parameters);
 	let button = document.createElement('button');
+	button.disabled = true;
+	let canBeShown = ['quiz', 'page', 'scorm', 'resource'].includes(parameters.currentModuleName);
 	button.textContent = 'сделать зашибись!';
 	button.addEventListener('click', ev=>{
-		setProperties(getId());
+		setProperties(parameters);
+		submit();
 	});
 	let form = document.querySelector('#mform1');
 	form.parentElement.insertBefore(button, form);
+	if(document.readyState == 'complete'){
+		if(canBeShown)
+			button.disabled = false;
+	}else{
+		window.addEventListener('load', ev=>{
+			if(canBeShown)
+				button.disabled = false;
+		});
+	}
 }
